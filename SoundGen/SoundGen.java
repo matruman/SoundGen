8a02a1a496a280835905cd236ea99cde65f897d9
unknown <ruslanzinewich@gmail.com>
2020-02-26 00:15:49 +0300

    some changes

--- SoundGen/SoundGen.java
+++ SoundGen/SoundGen.java
@@ -6,10 +6,9 @@ import java.util.concurrent.locks.ReentrantLock;
 
 class PlaySound extends Thread {
 	
-	public ReentrantLock 	locker;
+	public final ReentrantLock 	locker;
 	private SourceDataLine	line;
 	private AudioFormat		format;
-	private byte[]	buff;
 	private short	s;
 	private double	c;
 	private double	d;
@@ -24,6 +23,20 @@ class PlaySound extends Thread {
 	private double	tmax;
 	private double	time;
 
+	public PlaySound() {
+
+		locker = new ReentrantLock();
+		vol = 1.0;
+		flag = true;
+		depth = 0.3;
+		freq = 1000;
+		mfreq = 30;
+		m2freq = 1.0;
+		fmr = 44100;
+		tmax = 10000000000.0;
+		time = 0;
+	}
+	
 	public boolean getFlag() {
 		return flag;
 	}
@@ -32,23 +45,17 @@ class PlaySound extends Thread {
 		this.flag = flag;
 	}
 
-	public PlaySound() {
+	public void setVolume(double vol) {
 
-		vol = 1.0;
-		flag = true;
-		depth = 0.3;
-		freq = 1000;
-		mfreq = 30;
-		m2freq = 1;
-		fmr = 44100;
-		tmax = 10000000000.0;
-		time = 0;
+		if (vol >= 0.0 && vol <= 1.0)
+			this.vol = vol;
+		else
+			System.out.println("Incorrect volume");
 	}
-	
+
 	public void run() {
 
-		locker = new ReentrantLock();
-		buff = new byte[2];
+		byte[] buff = new byte[2];
 		if (line == null) {
 			format = new AudioFormat(
 					44100,
@@ -72,15 +79,18 @@ class PlaySound extends Thread {
 		line.start();
 		while (time < tmax && flag)
 		{
-			c = 32768 * vol * Math.sin((double)time * 2 * Math.PI * (freq / fmr));
-			d = 0.5 + 0.5 * Math.sin((double)time * 2 * Math.PI * (mfreq / fmr));
-			b = 0.5 + 0.5 * depth * Math.sin((double)time * 2 * Math.PI * (m2freq / fmr));
+			c = 32768 * vol * Math.sin(time * 2 * Math.PI * (freq / fmr));
+			d = 0.5 + 0.5 * Math.sin(time * 2 * Math.PI * (mfreq / fmr));
+			b = 0.5 + 0.5 * depth * Math.sin(time * 2 * Math.PI * (m2freq / fmr));
 			s = (short)(c *d * b);
 			buff[1] = (byte)(s & 0x00ff);
 			buff[0] = (byte)((s & 0xff00) >> 8);
 			line.write(buff, 0, 2);
-			locker.lock();
-			locker.unlock();
+			if ((long)(time - (fmr / mfreq) * 0.75) % (long)(fmr / mfreq) < 2)
+			{	
+				locker.lock();
+				locker.unlock();
+			}
 			time++;
 		}
 		line.drain();
@@ -99,6 +109,7 @@ public class SoundGen {
 		boolean	controllerFlag = true;
 		while(controllerFlag)
 		{
+			System.out.print("SoundGen> ");
 			command = scanner.nextLine();
 			if (command.equals("play")) {
 				try {	
